8e273dffeb4d3c3aff4b3b0e746605ee63ab3163
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-11 18:35:48 -0300

    ✨ Introducing docente and dicente forms

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import CadastroP from '@/views/CadastroP.vue';
+import CadastroA from '@/views/CadastroA.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,8 +13,13 @@ const router = createRouter({
     },
     {
       path: "/cadastrop",
-      name: "cadastro",
+      name: "cadastrop",
       component: CadastroP
+    },
+    {
+      path: "/cadastroa",
+      name: "cadastroa",
+      component: CadastroA
     }
   ]
 })
